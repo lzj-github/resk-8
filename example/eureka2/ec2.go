c72fb10e4ea9df552c3777e5e88c3e4814b46f6b
lzj-github <983470394@qq.com>
2020-11-11 09:21:59 +0800

    branch test

--- example/eureka2/ec2.go
+++ example/eureka2/ec2.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tietang/go-eureka-client/eureka"
 	"github.com/tietang/props/ini"
 )
 
 func main() {
-
+	fmt.Println("111")
 	conf := ini.NewIniFileConfigSource("ec.ini")
 	client := eureka.NewClient(conf)
 	client.Start()
